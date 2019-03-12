4014023bb1bb40d68d72d86286aa4b7a4b22fda3
MadsFrederiksen <MadsFrederiksen22@gmail.com>
2019-03-12 21:38:26 +0100

    Delete

--- dataproject/dataproject/Dataproject.py
+++ dataproject/dataproject/Dataproject.py
@@ -1,16 +1,2 @@
 message = "Hej Bjooorn"
 print(message)
-
-message2 = "jeg er sulten"
-print(message2)
-
-
-
-Message4 = "Fuck"
-print(Message4)
-
-fjams = "noget kode"
-print(fjams)
-
-changes = "Make some changes"
-print(changes)
