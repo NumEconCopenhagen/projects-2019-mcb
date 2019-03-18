58b79240b6b4ac34bc7c6ecee4e42a8bce5f9248
ChristianFogtdal <fogtal02@gmail.com>
2019-03-18 17:45:38 +0100

    chris

--- dataproject/dataproject/Dataproject.py
+++ dataproject/dataproject/Dataproject.py
@@ -6,9 +6,11 @@ print(message1)
 
 # Run this shit mah niggas in your terminal (no #)
 # Source: https://pypi.org/project/IneqPy/
-# git clone https://github.com/mmngreco/IneqPy.git
-# cd IneqPy
-# pip install .
+"""
+git clone https://github.com/mmngreco/IneqPy.git
+cd IneqPy
+pip install .
+"""
 
 message2 = "jeg hacker nu" 
 print(message2)
@@ -20,3 +22,32 @@ print(message2)
 import pandas as pd
 import numpy as np
 import ineqpy
+
+def G(v):
+    bins = np.linspace(0., 100., 11)
+    total = float(np.sum(v))
+    yvals = []
+    for b in bins:
+        bin_vals = v[v <= np.percentile(v, b)]
+        bin_fraction = (np.sum(bin_vals) / total) * 100.0
+        yvals.append(bin_fraction)
+    # perfect equality area
+    pe_area = np.trapz(bins, x=bins)
+    # lorenz area
+    lorenz_area = np.trapz(yvals, x=bins)
+    gini_val = (pe_area - lorenz_area) / float(pe_area)
+    return bins, yvals, gini_val
+
+v = np.random.rand(500)
+bins, result, gini_val = G(v)
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(bins, result, label="observed")
+plt.plot(bins, bins, '--', label="perfect eq.")
+plt.xlabel("fraction of population")
+plt.ylabel("fraction of wealth")
+plt.title("GINI: %.4f" %(gini_val))
+plt.legend()
+plt.subplot(2, 1, 2)
+plt.hist(v, bins=20)
+
