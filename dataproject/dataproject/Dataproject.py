908d0c0f9b4252461bc354b87a1ab038c3e28c63
= <nvf696@alumni.ku.dk>
2019-03-18 17:15:54 +0100

    IneqPy package

--- dataproject/dataproject/Dataproject.py
+++ dataproject/dataproject/Dataproject.py
@@ -3,3 +3,8 @@ print(messages)
 
 message1 = "Let the hacking begin"
 print(message1)
+
+# Run this shit mah niggas in your terminal
+# git clone https://github.com/mmngreco/IneqPy.git
+# cd IneqPy
+# pip install .
