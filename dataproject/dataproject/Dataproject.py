d8e7d56a37093b5e1e08c13e026741f6a832c19f
= <nvf696@alumni.ku.dk>
2019-03-18 17:20:38 +0100

    SYNC

--- dataproject/dataproject/Dataproject.py
+++ dataproject/dataproject/Dataproject.py
@@ -4,7 +4,7 @@ print(messages)
 message1 = "Let the hacking begin"
 print(message1)
 
-# Run this shit mah niggas in your terminal
+# Run this shit mah niggas in your terminal (no #)
 # git clone https://github.com/mmngreco/IneqPy.git
 # cd IneqPy
 # pip install .
