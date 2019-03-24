6a8be825818629eb8e81f5816d8dd0604617c098
MadsFrederiksen <MadsFrederiksen22@gmail.com>
2019-03-24 21:31:02 +0100

    Automating

--- dataproject/dataproject/Stock data first pass.py
+++ dataproject/dataproject/Stock data first pass.py
@@ -1,4 +1,4 @@
-#Stock data first pass
+#First pass
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -7,6 +7,10 @@ import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader as web
 import numpy as np
+import bs4 as bs
+import pickle
+import requests
+import os
 style.use("ggplot")
 
 #Tesla
@@ -64,3 +68,44 @@ ax1.xaxis_date()
 candlestick_ohlc(ax1,Tesla_ohlc.values, width=2, colorup="g")
 ax2.fill_between(Tesla_volume.index.map(mdates.date2num), Tesla_volume.values, 0)
 plt.show() #Candlestick and volume on the lower graph
+
+
+#Automating S&P500 
+def save_sp500_tickers():
+    resp = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find("table", {"class": "wikitable sortable"})
+    tickers = []
+    for row in table.findAll("tr")[1:]:
+        ticker = row.findAll("td")[0].text
+        tickers.append(ticker)
+
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+
+        print(tickers)
+
+        return(tickers)
+
+save_sp500_tickers()
+
+def data_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists("stock_dfs"):
+        os.makedirs("stock_dfs")
+
+start = (2013,1,1)
+end = (2018,12,31)
+
+for ticker in tickers:
+    if not os.path.exists("stock_dfs/{}.csv".format(ticker)):
+        df = web.DataReader(ticker, "yahoo", start)
+        df.to_csv("stock_dfs/{}.csv".format(ticker))
+    else:
+        print("Already have {}".format(ticker))
+
