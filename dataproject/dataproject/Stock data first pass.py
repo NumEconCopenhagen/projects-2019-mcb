9ef81120c360f55d0af3ce9824a9dd7f2900ad6f
MadsFrederiksen <MadsFrederiksen22@gmail.com>
2019-03-24 21:59:03 +0100

    Final automation

--- dataproject/dataproject/Stock data first pass.py
+++ dataproject/dataproject/Stock data first pass.py
@@ -99,15 +99,11 @@ def data_yahoo(reload_sp500=False):
     if not os.path.exists("stock_dfs"):
         os.makedirs("stock_dfs")
 
-    start = (2013,1,1)
-    end = (2016,12,31)
-
+    start = dt.datetime(2018,1,1)
+    end = dt.datetime.now()
     for ticker in tickers:
-    # just in case your connection breaks, we'd like to save our progress!
         if not os.path.exists("stock_dfs/{}.csv".format(ticker)):
             df = web.DataReader(ticker, "yahoo", start, end)
-            df.reset_index(inplace=True)
-            df.set_index("date", inplace=True)
             df.to_csv("stock_dfs/{}.csv".format(ticker))
         else:
             print("Already have {}".format(ticker))
