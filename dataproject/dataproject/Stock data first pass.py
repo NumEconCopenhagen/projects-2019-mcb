4718072b1f7b42ddc2b0d5c56aca41305835c32c
= <nvf696@alumni.ku.dk>
2019-03-25 16:12:36 +0100

    s&p 500 names

--- dataproject/dataproject/Stock data first pass.py
+++ dataproject/dataproject/Stock data first pass.py
@@ -89,6 +89,24 @@ def save_sp500_tickers():
 
 save_sp500_tickers()
 
+def save_sp500_names():
+    resp = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find("table", {"class": "wikitable sortable"})
+    names = []
+    for row in table.findAll('tr')[1:]:
+        name = row.findAll('td')[1].text.replace('.','-')
+        names.append(name)
+
+    with open("sp500names.pickle", "wb") as f:
+        pickle.dump(names, f)
+
+        print(names)
+
+        return(names)
+
+save_sp500_names()
+
 def data_yahoo(reload_sp500=False):
     if reload_sp500:
         tickers = save_sp500_tickers()
@@ -99,7 +117,7 @@ def data_yahoo(reload_sp500=False):
     if not os.path.exists("stock_dfs"):
         os.makedirs("stock_dfs")
 
-    start = dt.datetime(2018,1,1)
+    start = dt.datetime(2000,1,1)
     end = dt.datetime.now()
     for ticker in tickers:
         if not os.path.exists("stock_dfs/{}.csv".format(ticker)):
