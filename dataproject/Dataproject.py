139f897b9e8e614f94f951fde874167d787695f3
MadsFrederiksen <MadsFrederiksen22@gmail.com>
2019-05-19 19:55:27 +0200

    Da

--- dataproject/Dataproject.py
+++ dataproject/Dataproject.py
@@ -45,8 +45,12 @@ def save_sp500_tickers():
     table = soup.find("table", {"class": "wikitable sortable"})
     tickers = []
     for row in table.findAll("tr")[1:]:
-        ticker = row.findAll("td")[1].text.replace(".","-")
+        ticker = row.findAll("td")[0].text.replace(".","-")
+        ticker = ticker[:-1]
         tickers.append(ticker)
+
+    df_tickers = pd.DataFrame(tickers)
+    df_tickers.to_csv("tickers.csv")
 
     with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
@@ -55,12 +59,69 @@ def save_sp500_tickers():
 
         return(tickers)
     
-
 save_sp500_tickers()
 
+df_tickers = pd.read_csv("tickers.csv", index_col= 0)
+df_tickers.columns = ["Ticker"]
+df_tickers.head()
 
-#Getting data from Yahoo
-def data_yahoo(reload_sp500=False):
+
+def get_names():
+    resp = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find("table", {"class": "wikitable sortable"})
+    names = []
+    for row in table.findAll("tr")[1:]:
+        name = row.findAll("td")[1].text.replace(".","-")
+        names.append(name)
+
+    df_names = pd.DataFrame(names)
+    df_names.to_csv("names.csv")
+
+    print(names)
+
+get_names()
+
+df_names = pd.read_csv("names.csv", index_col= 0)
+df_names.columns = ["Name"]
+df_names.head()
+
+
+def get_sectors():
+    resp = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find("table", {"class": "wikitable sortable"})
+    sectors = []
+    for row in table.findAll("tr")[1:]:
+        sector = row.findAll("td")[3].text.replace(".","-")
+        sectors.append(sector)
+
+    df_sectors = pd.DataFrame(sectors)
+    df_sectors.to_csv("sectors.csv")
+
+    print(sectors)
+
+get_sectors()
+
+df_sectors = pd.read_csv("sectors.csv", index_col= 0)
+df_sectors.columns = ["Sector"]
+df_sectors.head()
+
+df_names_and_sector = df_names.join(df_sectors, how = "right")
+df_names_and_sector.head()
+
+df_tickers_names_sectors = df_tickers.join(df_names_and_sector, how = "right")
+df_tickers_names_sectors.head()
+#df_tickers_names_sectors.set_index("Ticker", inplace=True)
+#df_tickers_names_sectors.head()
+df_tickers_names_sectors.values.tolist
+
+with open ("tickers_names_sectors.pickle", "wb") as f:
+    pickle.dump(df_tickers_names_sectors, f)
+
+
+
+def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
         tickers = save_sp500_tickers()
     else:
@@ -69,18 +130,17 @@ def data_yahoo(reload_sp500=False):
     if not os.path.exists('stock_dfs'):
         os.makedirs('stock_dfs')
 
-    start = dt.datetime(2000, 1, 1)
+    start = dt.datetime(2010, 1, 1)
     end = dt.datetime.now()
     for ticker in tickers:
         # just in case your connection breaks, we'd like to save our progress!
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, 'yahoo', start, end)
+            df = web.DataReader(ticker, "yahoo", start, end) 
             df.to_csv('stock_dfs/{}.csv'.format(ticker))
         else:
             print('Already have {}'.format(ticker))
 
-data_yahoo()
-
+get_data_from_yahoo()
 
 def compile_data():
     with open("sp500tickers.pickle", "rb") as f:
@@ -95,21 +155,20 @@ def compile_data():
         df.set_index("Date", inplace=True)
         df.rename(columns = {"Adj Close": ticker}, inplace=True) #Adj Close takes the categories place in the column - Simple rename
         df.drop(["Open","High","Low","Close","Volume"],1, inplace=True)
-        df = df.divide(df.iloc[0])*100
-
+    
         if main_df.empty:
             main_df = df
         else:
             main_df = main_df.join(df, how="outer")
         
-        if count % 10 == 0: #Only print #10, #20, #30, etc.
+        if count % 100 == 0: #Only print #10, #20, #30, etc.
             print(count)
     print(main_df.head())
     main_df.to_csv("sp500_joined_adj_closes.csv")
 
 compile_data()
 
-df_stocks = pd.read_csv("sp500_joined_adj_closes.csv")
+data_df = pd.read_csv("sp500_joined_adj_closes.csv")
 df_stocks.set_index("Date", inplace=True)
 
 print(df_stocks)
